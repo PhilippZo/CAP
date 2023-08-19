4126eb41aa1a991d1ca41f9e0f17341eca3ca7e7
PilippZo <philipp.zolthoff@tu-dortmund.de>
2023-08-19 12:44:43 +0200

    stuff task2

--- poject/task2/test.py
+++ poject/task2/test.py
@@ -1,13 +1,31 @@
 import numpy as np 
-from dicompylercore import dicomparser, dvh, dvhcalc
+#from dicompylercore import dicomparser, dvh, dvhcalc
 from matplotlib import pyplot as plt 
 import matplotlib.cm as cm
 import seaborn as sns
+import pydicom
+import pandas as pd
+
+dcm_data = pydicom.dcmread('../../DoseCTV.dcm')
+#df  = pd.read_csv("../../DoseCTV.csv", header = None)
+
+scaling_factor = 100.0  # Modify this as needed
+
+print("Dose Scaling: ", dcm_data.DoseGridScaling)
+print(np.max(dcm_data.pixel_array[:]))
+
+# Scale the pixel values
+#dcm_data.pixel_array[:] = dcm_data.pixel_array * scaling_factor
+
+# Adjust the DoseGridScaling value
+dcm_data.DoseGridScaling = dcm_data.DoseGridScaling * 100000
+
+print("Dose Scaling: ", dcm_data.DoseGridScaling)
+
+# add new tag
+dcm_data.add_new((0x3004, 0x0002), 'CS', 'GY')
+
+# Save the modified DICOM to a new file
+dcm_data.save_as('Dose.dcm')
 
 
-dp = dicomparser.DicomParser("../../data/CT/DicomDaten/Dose.dcm")
-
-#dvh = dvh.DVH.from_dicom_dvh(dp, 0)
-
-structures = dp.GetStructures()
-print(structures)
